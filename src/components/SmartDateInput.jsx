4c95fe0c8dc64c8d55fd049f168d143001fbfd38
vishyvasadi-arch <vishyvasadi@gmail.com>
2026-03-16 15:45:47 +0530

    Update SmartDateInput.jsx

--- src/components/SmartDateInput.jsx
+++ src/components/SmartDateInput.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useState, useEffect } from 'react';
 import { parseAnyDate, formatDisplay, toInputVal } from '../utils/dateParser';
 import { isWeekend, DAY_NAMES } from '../utils/calculations';
